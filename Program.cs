cacd6af155672b13039e016128074a4091b9e30a
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-18 12:28:51 -0700

    new using ref

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int studentGrade = 75;
+        ExtraCreditRef(ref studentGrade);
+        Console.WriteLine(studentGrade);
+    
+    }
+    static void ExtraCreditRef(ref int studentGrade)
+    {
+            studentGrade +=3;
+
+    }
+}
