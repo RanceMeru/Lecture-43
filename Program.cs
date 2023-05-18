e0bca3e7c317834c4e9571ec025992bc8f7ea280
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-18 12:34:08 -0700

    new comments

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
     static void Main(string[] args)
     {
         int studentGrade = 75;
+        //passes location of memory by reference and not by memory
         ExtraCreditRef(ref studentGrade);
         Console.WriteLine(studentGrade);
     //for this example it would be the same with or without the ref keyword
